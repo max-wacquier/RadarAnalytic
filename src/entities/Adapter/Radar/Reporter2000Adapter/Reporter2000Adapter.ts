d985ce5ff6262d74abb5a3d2148421178899633b
max-wacquier <max.wacquier@gmail.com>
2023-06-19 17:43:22 +0200

    xml parser added

--- src/entities/Adapter/Radar/Reporter2000Adapter/Reporter2000Adapter.ts
+++ src/entities/Adapter/Radar/Reporter2000Adapter/Reporter2000Adapter.ts
@@ -3,6 +3,7 @@ import { Incident } from "../../../Incident/Incident";
 import { Radar } from "../../../Radar/Radar";
 import Reporter2000Base from "./Reporter2000Template.json"
 
+import * as fs from 'fs';
 const parser = require('xml2json');
 
 export class Reporter2000Adapter implements IRadarAdapter {
@@ -18,8 +19,30 @@ export class Reporter2000Adapter implements IRadarAdapter {
       }
     return true;
     }
-  
+
     createRadar (format: string): Radar {
-    return new Radar("","",0, [new Incident("", new Date, "","","","")]); //BOUCHONNAGE MOCHE
-    } 
+
+      const xml = fs.readFileSync(format, {encoding: 'utf-8'});
+      const list: any = xml.match(/"(.*?)"/g)
+  
+      const location = list[0].replaceAll('"', '')
+      const date = list[1].replaceAll('"', '')
+      let listOfIncidents: Array<Incident> = new Array<Incident>()
+  
+      let nbCurrentIncidentElements = 0
+      let currentIncident = []
+  
+      for (let index = 2; index < list.length; index++) {
+          if ((nbCurrentIncidentElements) < 3) {
+              currentIncident.push(list[index].replaceAll('"', ''))
+              nbCurrentIncidentElements++
+          } else if ( nbCurrentIncidentElements === 3) {
+              currentIncident.push(list[index].replaceAll('"', ''))
+              listOfIncidents.push(new Incident(currentIncident[0], date, currentIncident[3], currentIncident[1], currentIncident[2], undefined))
+              currentIncident = []
+              nbCurrentIncidentElements = 0
+          }
+      }
+      return new Radar(undefined, location, undefined, listOfIncidents);
+  }
 }
